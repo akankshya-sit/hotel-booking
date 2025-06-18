2c7ebe383bfeed18d6d66baae3ea9cb8e7cd94ab
Surajit Das <dassurajit59@yahoo.in>
2025-06-18 22:16:22 +0530

    clerk fix

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -2,7 +2,10 @@ import User from "../models/User.js";
 
 // Middleware to check if user is authenticated
 export const protect = async (req, res, next) => {
-  const { userId } = req.auth;
+  // const { userId } = req.auth;
+  const authHeader = req.header('Authorization')
+  const userId = authHeader.split(" ")[1]
+  console.log("User ID:", userId);
   if (!userId) {
     res.json({ success: false, message: "not authenticated" });
   } else {
